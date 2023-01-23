614c492cf42fbb4283fc1cda52e7449910461ec3
Marvin Clerc <marvin.clerc@edu.devinci.fr>
2023-01-23 09:45:12 +0100

    :art::sparkles::lipstick: update ui portfolio loading screen & navbar

--- CV.js
+++ CV.js
@@ -4,9 +4,16 @@ const opencvright = document.
 querySelector('.cassetteright')
 const opencvmid = document.
 querySelector('.CV')
+const navbar = document.
+querySelector('.P-navigation')
+
+window.onload = function() {
+     
+    var loadingScreen = document.getElementById("loading-screen");
+    loadingScreen.style.display = "none";
 
 
-console.log(opencv);
+};
 
 window.addEventListener('scroll', () => {
 
@@ -16,21 +23,38 @@ window.addEventListener('scroll', () => {
     console.log(scrollTop, clientHeight);
 
     const topelementtotop = opencv.getBoundingClientRect().top;
+    const topelementtobot = opencv.getBoundingClientRect().bottom;
+
 
     if(scrollTop > (scrollTop + topelementtotop).toFixed() -
     clientHeight * 0.30) {
         opencv.classList.add('active')
         opencvright.classList.add('active')
         opencvmid.classList.add('active')
-        
     }
 
-    if(scrollTop > (scrollTop + topelementtotop).toFixed() -
-    clientHeight * 0.1) {
-        
-        console.log("im gay");
-        
+    if(scrollTop > (scrollTop + topelementtobot).toFixed() -
+    clientHeight * 0.4) {
+        opencv.className ="Cassetteleft"
+        opencvright.className ="cassetteright"
+        opencvmid.className ="CV"
     }
-    
-    
+
+
+
+
+
+
 })
+window.onscroll = function() {
+    if (window.pageYOffset > 50) {
+        navbar.classList.add("active");
+        console.log("quoi? feur")
+    }
+    else {
+        navbar.className = "P-navigation"
+    }
+}
+
+
+
